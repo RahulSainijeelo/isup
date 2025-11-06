1f85bf61a735ed8433fa5fb6ad32f0c2b6ee5376
Rahul Saini <RahulSainijeelo>
2025-11-07 01:08:31 +0530

    added store

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import v1Router from "./routes/v1/index.routes.js"
 const app = express();
-
+import { prismaClient } from "@repo/store/client";
 app.use("/api/v1",v1Router)
 
 app.get("/website",(req,res)=>{
-
+  res.send("you reached here by chance")
 })
 
 
@@ -14,4 +14,4 @@ app.get("/status/:websiteId",(req,res)=>{
 })
 
 
-app.listen(process.env.PORT||3000)
+app.listen(process.env.PORT||8080)
